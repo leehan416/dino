36cc4f3612126b3804d310b6979c216953fdef00
Whal3 <35258105+leehan416@users.noreply.github.com>
2021-05-05 21:41:10 +0900

    .

--- src/dino/DinoController.java
+++ src/dino/DinoController.java
@@ -4,30 +4,62 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class DinoController {
+	boolean isjumping = false;
+	float speed = 100f;
+	
+	float jumpX = 0; //점프 컨트롤 할때 필요한x
+	float jump = 100f;
 
-	public static float speed = 100f; 
-	public static float jump = 100f; 
-	
-	
-	
-	
+	float dinoX;
+	float dinoY;
+
 	class key implements KeyListener {
 
 		@Override
 		public void keyTyped(KeyEvent e) {
 
-			if (e.getKeyCode() == 38) {//위 
+			if (e.getKeyCode() == 38) {// 위
 				System.out.println("!");
-			} else if (e.getKeyCode() == 40) { // 아래 
+			} else if (e.getKeyCode() == 40) { // 아래
 
-			} 
+			}
 		}
 
 		@Override
-		public void keyPressed(KeyEvent e) { }
+		public void keyPressed(KeyEvent e) {
+		}
 
 		@Override
-		public void keyReleased(KeyEvent e) { }
-
+		public void keyReleased(KeyEvent e) {
+		}
 	}
+
+	public void Move() {
+		dinoX += speed;
+	}
+
+	public void Jump() {
+		
+		dinoX = (float) (jump * Math.sin(jumpX));
+		jumpX += .6f;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		if (isjumping) {
+			
+		} else {
+			i
+		}
+	}
+
 }
--- src/dino/Main.java
+++ src/dino/Main.java
@@ -4,13 +4,18 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("test");
 		Ui ui = new Ui();
-		
+		DinoController dino = new DinoController();
 		
 		
 		//gameControll
 		while(true) {
 			try { // 초당 2 프레임
 				Thread.sleep(500);
+				
+				dino.Jump();
+				
+				dino.Move();
+				
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
